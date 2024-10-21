506668c8a50a448256125b07acb9215547866062
johnboshra88 <johnboshra10988@gmail.com>
2024-10-21 20:00:40 +0300

    newset books cubit done

--- lib/features/home/presentation/manger/newset_books_cubit/newset_books_cubit.dart
+++ lib/features/home/presentation/manger/newset_books_cubit/newset_books_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fechnewseteBook() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchFeatureBooks();
+
+    result.fold((failure) {
+      emit(NewsetBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewsetBooksSuccess(books));
+    });
+  }
 }
